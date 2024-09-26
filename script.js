4d440b9e0f69c900b87cabf7302358d0cf5bebae
Lucas Marques <lucasolivra@gmail.com>
2024-09-26 17:32:29 +0100

    Commit - Formatando o valor total

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+
+//Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
+
 //Fica de olho se tem algum evento no input. Captura o evento de input para formatar o valor
 amount.oninput = () => {
     //para remover letras, usamos o regex ( amount.value e o que eu digitei no input)
@@ -40,7 +46,113 @@ form.onsubmit = (event) => {
     }
 
     //fazendo o teste
-    console.log(newExpense);
-    
+    // console.log(newExpense);
+
+    //Chamando a função que irá adicionar o item na lista.
+    expenseAdd(newExpense)
+
+}
+// Adiciona um novo item na lista
+function expenseAdd (newExpense) {
+    try {
+        // Criar o elemento  para adicionar o item (li) na lista (ul)
+        const expenseItem = document.createElement("li")
+        //add a classe de estilização
+        expenseItem.classList.add("expense")
+
+        //Cria o icone da categoria
+        const expenseIcon = document.createElement("img")
+        //Criando os atributos da tag img 
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+        // Cria a div info da despesa
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        // Criando o nome da despesa
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        //Cria a categoria da despesa
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        // Adiciona o name e category na div das info das despesas
+        expenseInfo.append(expenseName, expenseCategory)
+
+        //Cria o valor da despesa
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$<small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+        // Cria o incone de remover
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
+        //Adiciona as informações no item.
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+        //adiciona o item na lista
+        expenseList.append(expenseItem)
+        
+        // Atualiza os totais
+        updateTotals(expenseItem)
+
+    } catch (error) {
+        alert("Não foi possivel atualizar a lista de despesas.")
+        console.log(error);
+        
+    }
 }
 
+// Altualizar os totais
+function updateTotals() {
+    try {
+        //Recupera todos os itens(li) da lista (ul)
+        const items = expenseList.children
+        console.log(items);
+
+        // Atualiza a quantiodade de itens na lista - If ternario, se quantidade de itens da lista miaor que 1 usa despesas se nao usa despesa
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+        //variável para incrementar o total.
+        let total = 0
+        //Percorre cada item (li ) da lista (ul)
+        for (let item = 0; item < items.length; item++){
+            const itemAmount = items[item].querySelector(".expense-amount")
+
+            //remove caracteres não numericos e substitui a virgula pelo ponto
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".")
+            //Converte o valor para float
+            value = parseFloat(value)
+
+            // VRF se é um numero valido.
+            if(isNaN(value)){
+                return alert("Não foi possível calcular o total. O valor nao arece ser um numero")
+            }   
+
+            //Incrmentar o valor total
+            total += Number(value)
+        }
+        
+        // Criar a span para add o R$ formatado 
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        // eu to formatando valor e removo o R$ que sera exibido pela smal, usei o toUpperCase para garantir que ele vai ta maiusculo
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$","")
+
+        //Limpa o conteudo do elemento
+        expensesTotal.innerHTML = ""
+
+        //Adiciona o simboloe da moeda eo valor total formatado
+        expensesTotal.append(symbolBRL, total)
+        
+    } catch (error) {
+        console.log(error);
+        alert("Não foi possivel atualiza os totais")
+        
+    }    
+}
